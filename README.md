6f9350700b7324d0bd991d76379bdc7b5db993bd
Robert Gabriel <robert_gabriel@outlook.com>
2016-03-22 20:39:40 +0000

    Added + command line + copy and past

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 ![alt text](assets/readme.png "Firebase Yeti")
 # About
-This project does what it says on the tin. Create a dmg of a website so you can have it as a quick shortcut in the dock on osx.
+This project does what it says on the tin. Wrap a website so you can have it as a quick shortcut in the dock on osx.
 
 # Installation
 
@@ -12,22 +12,31 @@ npm install websitewrapper
 # Usage
 You need to pass three things into it.
 
-1. The url of the website you want to create the shortcut for.
+1. -u :  The url of the website you want to create the shortcut for.
 
-2. The title of the website.
+2. -t : The title of the website.
 
-3. The icon (its full path) you want pass (To convert images to icns visit [iconverticons](https://iconverticons.com/online/) )
+3. -i : The icon (its full path) you want pass (To convert images to icns visit [iconverticons](https://iconverticons.com/online/) )
 
-It will then create a folder called build . Mac app (.app) wrapper are there now  :)
+It will then create a folder called build on your desktop. Everything is in there now.
 
-
+## Command
+```shell
+websitewrapper -u http://www.google.ie -t Google -i /Users/robertjgabriel/desktop/icon.icn
 ```
+
+
+## Code
+```javascript
 var websitewrapper = require('websitewrapper'),
 
   websitewrapper.create("http://www.google.ie","batman","yeti.icns");
 
-
 ```
+
+# Support
+Osx at the moment.
+Feel Free to make a pull request for a windows version.
 
 # Example
 As you can see
--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var NwBuilder = require('nw-builder');
 var move = require('file-move');
 var rename = require("gulp-rename");
 var gulp = require('gulp');
-
+var osenv = require('osenv');
+var username =  require('username');
 
 /**
  * Create A HTML Wrapper for nicer logos
@@ -18,12 +19,16 @@ module.exports = {
 
   create: function(url, title, icons) {
 
+    username().then(username => {
+
+
+
 
     gulp.src(icons)
       .pipe(rename("icon.icns"))
       .pipe(gulp.dest(__dirname + '/temp'));
 
-    fs.writeFile('./temp/index.html', '<!DOCTYPE html><html lang="en"><head style="border:0px;margin:0px;"><title>' + title + '</title></head><body style="border:0px;margin:0px;"><iframe src="' + url + '" style="border:0;width:100vw; height:100vw;padding:0;"></iframe></body></html>', function(err) {
+    fs.writeFile('./temp/index.html', '<!DOCTYPE html><html lang="en"><head style="border:0px;margin:0px;"><title>' + title + '</title><script>var gui=require("nw.gui"),menu=new gui.Menu({type:"menubar"}),menuItems=new gui.Menu;menu.createMacBuiltin("'+ title+ '",{hideEdit:!1,hideWindow:!0}),gui.Window.get().menu=menu;</script></head><body style="border:0px;margin:0px;"><iframe src="' + url + '" style="border:0;width:100vw; height:100vw;padding:0;"></iframe></body></html>', function(err) {
       if (err) {
         return console.log(err);
       }
@@ -35,7 +40,7 @@ module.exports = {
       platforms: ['osx64', 'win64'],
       appName: title,
       macIcns: "./temp/icon.icns",
-      buildDir: __dirname + "/build",
+      buildDir: "/Users/"+ username + "/Desktop/" + "/build",
       version: "0.12.0",
       zip: false
 
@@ -48,5 +53,7 @@ module.exports = {
     }).catch(function(error) {
       console.error(error);
     });
+    });
   }
+
 };
