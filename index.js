ea876e60f32d6b99667f5b4175901ed1c7044060
Robert Gabriel <robert_gabriel@outlook.com>
2016-04-19 14:37:56 +0100

    Added build paths

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #! /usr/local/bin/node
+
 var fs = require('fs');
 var NwBuilder = require('nw-builder');
 var move = require('file-move');
@@ -19,6 +20,9 @@ module.exports = {
    * @return {none} none
    */
   create: function(url, title, icons) {
+    var platform = process.platform;
+    var loggedInUsername = null;
+    var buildPath = null;
 
     var html = '<!DOCTYPE html>' +
       '<html lang="en">' +
@@ -33,57 +37,73 @@ module.exports = {
       '</body>' +
       '</html>';
 
-
-    // Gets thee current username of the user logged in
+    /**
+     * Gets the current username that is logged in... Not root if in sudo mode
+     * @param  {String} Path to the icons
+     */
     username().then(username => {
-
-      /**
-       * Create A HTML Wrapper for nicer logos
-       * @param  {String} Path to the icons
-       */
-      gulp.src(icons)
-        .pipe(rename("icon.icns"))
-        .pipe(gulp.dest(__dirname + '/temp'));
-
-
-      /**
-       * Move the Files and create the Html
-       * @param  {String} Path to the icons
-       */
-      fs.writeFile(__dirname + '/temp/index.html', html, function(err) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log("The file was saved!");
-      });
-
-
-      /**
-       * Nw.js Build system to create the files.
-       * @param  {String} Path to the icons
-       */
-      var nw = new NwBuilder({
-        files: [__dirname + '/temp/index.html', __dirname + '/temp/icon.icns', __dirname + '/temp/package.json', __dirname + 'node_modules/**'], // use the glob format
-        platforms: ['osx64', 'win64'],
-        appName: title,
-        macIcns: __dirname + "/temp/icon.icns",
-        buildDir: "/Users/" + username + "/Desktop/" + "/build",
-        version: "0.12.0",
-        zip: false
-      });
-
-
-      /**
-       * Log system information
-       * @param  {none} none
-       */
-      nw.build().then(function() {
-        console.log(chalk.green.bold("All Done!!! Enjoy " + chalk.red("<3")));
-      }).catch(function(error) {
-        console.log(chalk.red.bold("Failed"));
-      });
-
-
+      loggedInUsername = username;
     });
+
+
+    /**
+     * Gets the platform and the the build path for windows and mac
+     * @param  {String} Path to the icons
+     */
+    if (platform === "darwin") {
+      buildPath = "/Users/" + loggedInUsername + "/Desktop/" + "build";
+    } else if (platform === "win32") {
+      buildPath = "c:\\Users\\" + loggedInUsername + "\\Desktop\\" + "build";
+    } else {
+      return console.log(chalk.red.bold("not supported platform"));
+    }
+
+
+    /**
+     * Create A HTML Wrapper for nicer logos
+     * @param  {String} Path to the icons
+     */
+    gulp.src(icons)
+      .pipe(rename("icon.icns"))
+      .pipe(gulp.dest(__dirname + '/temp'));
+
+
+    /**
+     * Move the Files and create the Html
+     * @param  {String} Path to the icons
+     */
+    fs.writeFile(__dirname + '/temp/index.html', html, function(err) {
+      if (err) {
+        return console.log(chalk.red.bold(err));
+      }
+      console.log(chalk.green.bold("The file was saved!"));
+    });
+
+
+    /**
+     * Nw.js Build system to create the files.
+     * @param  {String} Path to the icons
+     */
+    var nw = new NwBuilder({
+      files: [__dirname + '/temp/index.html', __dirname + '/temp/icon.icns', __dirname + '/temp/package.json', __dirname + 'node_modules/**'], // use the glob format
+      platforms: ['osx64', 'win64'],
+      appName: title,
+      macIcns: __dirname + "/temp/icon.icns",
+      buildDir: buildPath,
+      version: "0.12.0",
+      zip: false
+    });
+
+
+    /**
+     * Log system information
+     * @param  {none} none
+     */
+    nw.build().then(function() {
+      console.log(chalk.green.bold("All Done!!! Enjoy " + chalk.red("<3")));
+    }).catch(function(error) {
+      console.log(chalk.red.bold("Failed"));
+    });
+    
   }
 };
