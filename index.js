71ddce7a4cfde05f9376adf38a358d410cbc0195
Robert Gabriel <robert_gabriel@outlook.com>
2016-05-03 15:23:46 +0100

    back to username

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #! /usr/local/bin/nodex
+
 var fs = require('fs');
 var NwBuilder = require('nw-builder');
 var move = require('file-move');
@@ -6,7 +7,7 @@ var rename = require("gulp-rename");
 var gulp = require('gulp');
 var osenv = require('osenv');
 var chalk = require('chalk');
-
+var username = require('username');
 
 module.exports = {
 
@@ -19,7 +20,7 @@ module.exports = {
    */
   create: function(url, title, icons) {
     var platform = process.platform;
-    var buildPath = null;// Set to null before the Operating system is detected.
+    var buildPath = null; // Set to null before the Operating system is detected.
 
     var html = '<!DOCTYPE html>' +
       '<html lang="en">' +
@@ -36,65 +37,74 @@ module.exports = {
 
 
     /**
-     * Gets the platform and the the build path for either windows and mac
+     * Gets the current username that is logged in... Not root if in sudo mode
      * @param  {String} Path to the icons
      */
-
-    if (platform === "darwin") { //Mac
-      buildPath = process.env.HOME + "/Desktop/" + "build";
-    } else if (platform === "win32") {
-      buildPath =  process.env.HOMEDRIVE + process.env.HOMEPATH  + "\\Desktop\\" + "build";
-    } else {
-      return console.log(chalk.red.bold("not supported platform"));
-    }
+    username().then(username => {
 
 
-    /**
-     * Create A HTML Wrapper for nicer logos
-     * @param  {String} Path to the icons
-     */
-    gulp.src(icons)
-      .pipe(rename("icon.icns"))
-      .pipe(gulp.dest(__dirname + '/temp'));
 
-
-    /**
-     * Move the Files and create the Html
-     * @param  {String} Path to the icons
-     */
-    fs.writeFile(__dirname + '/temp/index.html', html, function(err) {
-      if (err) {
-        return console.log(chalk.red.bold(err));
+      /**
+       * Gets the platform and the the build path for either windows and mac
+       * @param  {String} Path to the icons
+       */
+      if (platform === "darwin") { //Mac
+        buildPath = "/Users/" + username + "/Desktop/" + "build";
+      } else if (platform === "win32") {
+        buildPath = process.env.HOMEDRIVE + process.env.HOMEPATH + "\\Desktop\\" + "build";
+      } else {
+        return console.log(chalk.red.bold("not supported platform"));
       }
-      console.log(chalk.green.bold("The file was saved!"));
-    });
 
 
-    /**
-     * Nw.js Build system to create the files.
-     * @param  {String} Path to the icons
-     */
-    var nw = new NwBuilder({
-      files: [__dirname + '/temp/index.html', __dirname + '/temp/icon.icns', __dirname + '/temp/package.json', __dirname + 'node_modules/**'], // use the glob format
-      platforms: ['osx64', 'win64'],
-      appName: title,
-      macIcns: __dirname + "/temp/icon.icns",
-      buildDir: buildPath,
-      version: "0.12.0",
-      zip: false
-    });
+      /**
+       * Create A HTML Wrapper for nicer logos
+       * @param  {String} Path to the icons
+       */
+      gulp.src(icons)
+        .pipe(rename("icon.icns"))
+        .pipe(gulp.dest(__dirname + '/temp'));
 
 
-    /**
-     * Log system information
-     * @param  {none} none
-     */
-    nw.build().then(function() {
-      console.log(chalk.green.bold("All Done!!! Enjoy " + chalk.red("<3")));
-    }).catch(function(error) {
-      console.log(chalk.underline.red.bold("Failed"));
-      console.log(chalk.blue(error));
-    });
+      /**
+       * Move the Files and create the Html
+       * @param  {String} Path to the icons
+       */
+      fs.writeFile(__dirname + '/temp/index.html', html, function(err) {
+        if (err) {
+          return console.log(chalk.red.bold(err));
+        }
+        console.log(chalk.green.bold("The file was saved!"));
+      });
 
+
+      /**
+       * Nw.js Build system to create the files.
+       * @param  {String} Path to the icons
+       */
+      var nw = new NwBuilder({
+        files: [__dirname + '/temp/index.html', __dirname + '/temp/icon.icns', __dirname + '/temp/package.json', __dirname + 'node_modules/**'], // use the glob format
+        platforms: ['osx64', 'win64'],
+        appName: title,
+        macIcns: __dirname + "/temp/icon.icns",
+        buildDir: buildPath,
+        version: "0.12.0",
+        zip: false
+      });
+
+
+      /**
+       * Log system information
+       * @param  {none} none
+       */
+      nw.build().then(function() {
+        console.log(chalk.green.bold("All Done!!! Enjoy " + chalk.red("<3" + buildPath)));
+      }).catch(function(error) {
+        console.log(chalk.underline.red.bold("Failed"));
+        console.log(chalk.blue(error));
+      });
+
+
+  });
   }
 };
